c570cabeef4262952cc7fabe6debffef0818d8b7
shadowwang <906103494@qq.com>
2017-10-12 20:41:48 +0800

    init project

---
+++ app/src/main/java/com/parsonswang/zxfootball/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.parsonswang.zxfootball;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by parsonswang on 2017/10/12.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
