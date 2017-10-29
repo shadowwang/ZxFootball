66a937f02c8949aee80a7f4c62cba8bd1680528b
wangchun <906103494@qq.com>
2017-10-29 17:34:11 +0800

    pinheader rv

---
+++ common/src/main/java/com/parsonswang/common/view/pinheader/AdapterStick.java
@@ -0,0 +1,21 @@
+package com.parsonswang.common.view.pinheader;
+
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.ViewGroup;
+
+/**
+ * Created by wubo on 2017/8/22.
+ */
+
+public interface AdapterStick<T extends ViewHolder> extends Stick{
+
+    ViewHolder createViewHolder(ViewGroup parent, int viewType);
+
+    int getItemViewType(int position);
+
+    int getItemCount();
+
+    void bindViewHolder(T holder, int position);
+
+    int getHeaderCount();
+}
---
+++ common/src/main/java/com/parsonswang/common/view/pinheader/Stick.java
@@ -0,0 +1,11 @@
+package com.parsonswang.common.view.pinheader;
+
+/**
+ * Created by wubo on 2017/8/22.
+ */
+
+public interface Stick {
+
+    boolean isPinnedViewType(int viewType);
+
+}
