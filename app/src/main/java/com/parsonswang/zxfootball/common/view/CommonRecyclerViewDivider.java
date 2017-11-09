be12734541aea58ddfcdba814d6ef23384186f77
shadowwang <906103494@qq.com>
2017-11-09 21:30:16 +0800

    加上通用diver

---
+++ app/src/main/java/com/parsonswang/zxfootball/common/view/CommonRecyclerViewDivider.java
@@ -0,0 +1,182 @@
+package com.parsonswang.zxfootball.common.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+
+/**
+ * Created by parsonswang on 2017/9/15.
+ */
+
+public class CommonRecyclerViewDivider extends RecyclerView.ItemDecoration {
+    private Paint mPaint;
+    private Drawable mDivider;
+    private int mLeftMargin;
+    private int mRightMargin;
+
+    private int mDividerLeftMargin = 0;
+    private int mDividerRightMargin = 0;
+
+    private int mDividerHeight = 1;//分割线高度，默认为1px
+    private int mOrientation;//列表的方向：LinearLayoutManager.VERTICAL或LinearLayoutManager.HORIZONTAL
+    private static final int[] ATTRS = new int[]{android.R.attr.listDivider};
+
+    int lastPos = -1;
+    int currentPos = 0;
+    int headCount = 0;
+    int tailCount = 0;
+
+
+    public void setTailCount(int tailCount) {
+        this.tailCount = tailCount;
+    }
+
+    /**
+     * 默认分割线：高度为2px，颜色为灰色
+     *
+     * @param context
+     * @param orientation 列表方向
+     */
+    public CommonRecyclerViewDivider(Context context, int orientation) {
+        if (orientation != LinearLayoutManager.VERTICAL && orientation != LinearLayoutManager.HORIZONTAL) {
+            throw new IllegalArgumentException("请输入正确的参数！");
+        }
+        mOrientation = orientation;
+
+        final TypedArray a = context.obtainStyledAttributes(ATTRS);
+        mDivider = a.getDrawable(0);
+        a.recycle();
+    }
+
+    /**
+     * 自定义分割线
+     *
+     * @param context
+     * @param orientation 列表方向
+     * @param drawableId  分割线图片
+     */
+    public CommonRecyclerViewDivider(Context context, int orientation, int drawableId) {
+        this(context, orientation);
+        mDivider = ContextCompat.getDrawable(context, drawableId);
+        mDividerHeight = mDivider.getIntrinsicHeight();
+    }
+
+    /**
+     * 自定义分割线
+     *
+     * @param context
+     * @param orientation   列表方向
+     * @param dividerHeight 分割线高度
+     * @param dividerColor  分割线颜色
+     */
+    public CommonRecyclerViewDivider(Context context, int orientation, int dividerHeight, int dividerColor) {
+        this(context, orientation);
+        mDividerHeight = dividerHeight;
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(dividerColor);
+        mPaint.setStyle(Paint.Style.FILL);
+    }
+
+    /**
+     * 自定义分割线,这个方法设置的margin只是控制分割线，不控制内容
+     * @param context
+     * @param orientation
+     * @param dividerHeight
+     * @param dividerLeftMargin
+     * @param dividerRightMargin
+     * @param dividerColor
+     */
+    public CommonRecyclerViewDivider(int orientation, int dividerHeight,
+                                     int dividerLeftMargin, int dividerRightMargin, int dividerColor, Context context) {
+        this(context, orientation, dividerHeight, dividerColor);
+        mDividerLeftMargin = dividerLeftMargin;
+        mDividerRightMargin = dividerRightMargin;
+    }
+
+    /**
+     * 自定义分割线，这个方法设置的margin会包括内容一起缩进
+     * @param context
+     * @param orientation
+     * @param dividerHeight
+     * @param leftMargin
+     * @param rightMargin
+     * @param dividerColor
+     */
+    public CommonRecyclerViewDivider(Context context, int orientation, int dividerHeight,
+                                     int leftMargin, int rightMargin, int dividerColor) {
+        this(context, orientation, dividerHeight, dividerColor);
+        mLeftMargin = leftMargin;
+        mRightMargin = rightMargin;
+    }
+
+    //获取分割线尺寸
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        super.getItemOffsets(outRect, view, parent, state);
+        outRect.set(mLeftMargin, 0, mRightMargin, mDividerHeight);
+    }
+
+    //绘制分割线
+    @Override
+    public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        super.onDraw(c, parent, state);
+        if (mOrientation == LinearLayoutManager.VERTICAL) {
+            drawVertical(c, parent);
+        } else {
+            drawHorizontal(c, parent);
+        }
+    }
+
+    //绘制横向 item 分割线
+    private void drawHorizontal(Canvas canvas, RecyclerView parent) {
+        final int left = parent.getPaddingLeft();
+        final int right = parent.getMeasuredWidth() - parent.getPaddingRight();
+        final int childSize = parent.getChildCount();
+        for (int i = 0; i < childSize; i++) {
+            final View child = parent.getChildAt(i);
+            int childPosInAdapter = parent.getChildAdapterPosition(child);
+            if(childPosInAdapter < headCount){
+                continue;
+            }
+            RecyclerView.LayoutParams layoutParams = (RecyclerView.LayoutParams) child.getLayoutParams();
+            final int top = child.getBottom() + layoutParams.bottomMargin;
+            final int bottom = top + mDividerHeight;
+            if (mDivider != null) {
+                mDivider.setBounds(left, top, right, bottom);
+                mDivider.draw(canvas);
+            }
+            if (mPaint != null) {
+                canvas.drawRect(mDividerLeftMargin+left, top, right-mDividerRightMargin, bottom, mPaint);
+            }
+        }
+    }
+
+    //绘制纵向 item 分割线
+    private void drawVertical(Canvas canvas, RecyclerView parent) {
+        final int top = parent.getPaddingTop();
+        final int bottom = parent.getMeasuredHeight() - parent.getPaddingBottom();
+        final int childSize = parent.getChildCount();
+        for (int i = 0; i < childSize; i++) {
+            final View child = parent.getChildAt(i);
+            RecyclerView.LayoutParams layoutParams = (RecyclerView.LayoutParams) child.getLayoutParams();
+            final int left = child.getRight() + layoutParams.rightMargin;
+            final int right = left + mDividerHeight;
+            if (mDivider != null) {
+                mDivider.setBounds(left, top, right, bottom);
+                mDivider.draw(canvas);
+            }
+            if (mPaint != null) {
+                canvas.drawRect(left, top, right, bottom, mPaint);
+            }
+        }
+    }
+
+}
