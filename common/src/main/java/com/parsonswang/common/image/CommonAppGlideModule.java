9672dd1f84ea0b68709e50a7c3433360a618cbc5
shadowwang <906103494@qq.com>
2017-12-07 13:45:17 +0800

    glide配置

--- common/src/main/java/com/parsonswang/common/image/CommonAppGlideModule.java
+++ common/src/main/java/com/parsonswang/common/image/CommonAppGlideModule.java
@@ -1,11 +1,23 @@
 package com.parsonswang.common.image;
 
+import android.content.Context;
+
+import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.DecodeFormat;
+import com.bumptech.glide.load.engine.cache.LruResourceCache;
 import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.request.RequestOptions;
 
 /**
  * Created by parsonswang on 2017/11/7.
  */
 @GlideModule
 public class CommonAppGlideModule extends AppGlideModule {
+
+    @Override
+    public void applyOptions(Context context, GlideBuilder builder) {
+        builder.setMemoryCache(new LruResourceCache(10 * 1024 * 1024));
+        builder.setDefaultRequestOptions(new RequestOptions().format(DecodeFormat.PREFER_RGB_565));
+    }
 }
