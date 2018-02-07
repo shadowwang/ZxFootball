0084bbcf34ef63ced66f8e3706d3a46a751ef7df
shadowwang <906103494@qq.com>
2018-02-07 21:19:31 +0800

    比赛头部信息处理

--- app/src/main/java/com/parsonswang/zxfootball/matches/detail/MatchDetailHerderFragment.java
+++ app/src/main/java/com/parsonswang/zxfootball/matches/detail/MatchDetailHerderFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.parsonswang.common.base.BaseFragment;
+import com.parsonswang.zxfootball.R;
 
 /**比赛头部信息Fragment
  * Created by parsonswang on 2018/2/6.
@@ -16,6 +17,7 @@ public class MatchDetailHerderFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_matchdetail_header, container, false);
+        return view;
     }
 }
