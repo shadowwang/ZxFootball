88d8330bfd41f862a288eb46ba6281edbdab4052
wangchun <906103494@qq.com>
2018-04-16 00:13:35 +0800

    比赛进程资源数据初始化

--- app/src/main/java/com/parsonswang/zxfootball/common/Constant.java
+++ app/src/main/java/com/parsonswang/zxfootball/common/Constant.java
@@ -35,7 +35,11 @@ public class Constant {
         public static final int EVENTTYPE_GOAL= 0;
         public static final int EVENTTYPE_ASSIST= 1;
         public static final int EVENTTYPE_GOAL_DIAN= 4;
+        public static final int EVENTTYPE_YELLOW_CARD = 7;
+        public static final int EVENTTYPE_2YELLOW_TO_RED = 9;//两黄被罚下
+        public static final int EVENTTYPE_TO_RED = 10;//红牌直接被罚下
         public static final int EVENTTYPE_SUBSTITUTES_DOWN = 11;
         public static final int EVENTTYPE_SUBSTITUTES_UP = 12;
+        public static final int EVENTTYPE_DEADLINESS = 16;//致命失误导致失球
     }
 }
