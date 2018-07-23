40bb39e8c3e0c45ed5f7e59a23b14d95dd56a249
wangchun <906103494@qq.com>
2018-07-23 22:29:26 +0800

    修复详情数据获取

--- app/src/main/java/com/parsonswang/zxfootball/matches/detail/MatchStatFetchDataCallback.java
+++ app/src/main/java/com/parsonswang/zxfootball/matches/detail/MatchStatFetchDataCallback.java
@@ -58,7 +58,7 @@ public class MatchStatFetchDataCallback extends HtmlCallback {
         MatchStatBean matchStatBean = new MatchStatBean();
 
         Elements elements = document.getElementsByTag("script");
-        Elements jsValElements = document.getElementsByTag("script").eq(22);
+        Elements jsValElements = document.getElementsByTag("script").eq(19);
 
         for (Element element : jsValElements) {
             //var表达式：var keyName = values;
