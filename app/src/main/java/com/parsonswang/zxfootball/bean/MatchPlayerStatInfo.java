758b3f996b024191197b4570452bbdc9f7b4cc54
wangchun <906103494@qq.com>
2018-11-20 23:26:39 +0800

    客队替补球员展示

--- app/src/main/java/com/parsonswang/zxfootball/bean/MatchPlayerStatInfo.java
+++ app/src/main/java/com/parsonswang/zxfootball/bean/MatchPlayerStatInfo.java
@@ -1,11 +1,31 @@
 package com.parsonswang.zxfootball.bean;
 
+import java.util.ArrayList;
+
 public class MatchPlayerStatInfo {
     public TeamInfo homeTeamInfo;
     public TeamInfo awayTeamInfo;
+
+    public ArrayList<PlayerStatInfo> homePlayerInfo;
+    public ArrayList<PlayerStatInfo> awayPlayerInfo;
 
     private class TeamInfo {
         public String teamLogo;
         public String team;
     }
+
+    private class PlayerStatInfo {
+        public String name;
+        public String num;
+        public String location;
+        public int shootCnt;
+        public int shootOnTargetCnt;
+        public int keyPass;
+        public float psPercent;
+        public int makeChanceCnt;
+        public int headingDuelCnt;
+        public int touchBallCnt;
+        public float scoreGrade;
+    }
+
 }
